cff402b1e7d04850fe3082002c53d0e21b6c5dc0
oixm <oixm@hotmail.com>
2013-06-23 18:44:45 +0800

    Slide Admin

--- App_Code/Slide.cs
+++ App_Code/Slide.cs
@@ -15,4 +15,27 @@ public class Slide
         var db = MyDB.Open();
         return db.Query("Select * From Slide Order By Sort");
     }
+
+    /// <summary>
+    /// 更新幻灯片
+    /// </summary>
+    public static void Update(int ID, string Title, string Link, int Sort)
+    {
+        var db = MyDB.Open();
+        var sql = @"Update Slide Set
+                    Title = @1,
+                    Link = @2,
+                    Sort = @3
+                    Where ID = @0";
+        db.Execute(sql, ID, Title, Link, Sort);
+    }
+    
+    /// <summary>
+    /// 删除幻灯片
+    /// </summary>
+    public static void Delete(int ID)
+    {
+        var db = MyDB.Open();
+        db.Execute("Delete From Slide Where ID = @0", ID);
+    }
 }
